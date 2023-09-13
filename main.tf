9f79541d8f01ef6e59c0052b7911875c7f305000
Tiny_V <374176008@qq.com>
2023-09-13 13:53:49 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = "1.209.1"
+    }
+  }
+}
+
+provider "alicloud" {
+  region       =   "cn-beijing"
+  access_key   =   "ccddde"
+  secret_key   =   "aabbc"
+}
+
+resource "alicloud_disk" "ecs_disk" {
+  zone_id = "cn-beijing"
+  disk_name              = "mayue-terraform-test"
+  description       = "mayue terraform test disk"
+  category          = "cloud_efficiency"
+  size              = "40"
+  tags = {
+    Name = "TerraformTest"
+  }
+}
